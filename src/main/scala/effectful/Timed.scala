c728cc8589b48383a1d58307f9a24fffa19978cc
Leandro Galvan <leusgalvan@gmail.com>
2022-05-06 14:24:06 -0300

    Add delayBy to timed and improve debounce example

--- src/main/scala/effectful/Timed.scala
+++ src/main/scala/effectful/Timed.scala
@@ -9,10 +9,11 @@ import scala.concurrent.duration._
 object Timed extends IOApp.Simple {
   override def run: IO[Unit] = {
     val drinkWater = Stream.iterateEval(1)(n => IO.println("Drink more water!").as(n+1))
-    drinkWater.compile.drain
 
+    drinkWater.compile.drain
     drinkWater.timeout(1.second).compile.drain
     drinkWater.interruptAfter(1.second).compile.drain
+    drinkWater.delayBy(2.seconds).interruptAfter(4.seconds)
 
     // Throttling
     drinkWater
@@ -22,12 +23,14 @@ object Timed extends IOApp.Simple {
       .toList
       .flatMap(IO.println)
 
+
     // Debounce
-    drinkWater
-      .debounce(1.second)
-      .interruptAfter(5.seconds)
+    val resizeEvents = Stream.iterate((0, 0)) { case (w, h) => (w + 1, h + 1) }.covary[IO]
+    resizeEvents
+      .debounce(500.millis)
+      .evalMap { case (h, w) => IO.println(s"Resizing window to height $h and width $w") }
+      .interruptAfter(3.seconds)
       .compile
-      .toList
-      .flatMap(IO.println)
+      .drain
   }
 }
